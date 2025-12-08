53fe2a0472cb3ca43ab646c2cd501c0a14c11406
Tharick Abreu <151549770+TkAbreu@users.noreply.github.com>
2025-12-08 17:37:55 +0000

    adiciona página inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Nega: Vocês são tudo pra mim e tudo que eu faço é pra dar o melhor para vocês. Amo vocês mais que tudo na minha vida!</h1>
+}
+
+export default Home;
